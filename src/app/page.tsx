e9d6e258ba03ad2f6af73a8f73d913ce1a964ec7
ahmadalmuhtar <ahmadalmuhtar@gmail.com>
2024-03-10 01:34:48 +0100

    finished the post page when authenticated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,16 @@ import {
   createPost,
   createUser,
   deleteUserById,
+  getPosts,
   getUsers,
 } from "./server/queries";
-import { User } from "@prisma/client";
+import { Post, User } from "@prisma/client";
 import { useSession } from "next-auth/react";
 
 export default function Home() {
   const [openAddForm, setOpenAddForm] = useState(false);
   const [users, setUsers] = useState<User[]>();
+  const [posts, setPosts] = useState<Post[]>();
   const [stateChanged, setStateChanged] = useState(false);
   const [openFormPost, setOpenFormPost] = useState(true);
   const [userData, setUserData] = useState<CreateUserPayload>({
@@ -25,6 +27,7 @@ export default function Home() {
     title: "",
     content: "",
     published: false,
+    authorId: 21,
   });
   const { data: session, status } = useSession();
 
@@ -33,8 +36,14 @@ export default function Home() {
     setUsers(allUsers);
   };
 
+  const getAllPosts = async () => {
+    const allPosts = await getPosts();
+    setPosts(allPosts);
+  };
+
   useEffect(() => {
     getAllUsers();
+    getAllPosts();
   }, [stateChanged]);
 
   const handleDelete = async (id: number) => {
@@ -53,141 +62,145 @@ export default function Home() {
   const handleAddPost = async (e: React.FormEvent) => {
     e.preventDefault();
     await createPost(postData);
+    console.log(postData);
     console.log(session?.user);
-    setPostData({ title: "", content: "", published: false });
+    setPostData({ title: "", content: "", published: false, authorId: 21 });
     setOpenFormPost(false);
   };
+  console.log(status);
 
   return (
-    <>
-      <div className="flex py-24 justify-center">
-        {openAddForm === true ? (
-          <form
-            onSubmit={handleAddUser}
-            className="flex grid-cols-2 space-x-10"
+    <div className="flex py-24 justify-center">
+      {openAddForm ? (
+        <form onSubmit={handleAddUser} className="flex grid-cols-2 space-x-10">
+          <button className="border border-spacing-3" type="submit">
+            Add user
+          </button>
+          <label>
+            User name:
+            <input
+              placeholder="Enter a new username"
+              className="border m-2"
+              onChange={(e) =>
+                setUserData((prev) => ({ ...prev, name: e.target.value }))
+              }
+              type="text"
+            />
+          </label>
+          <label>
+            Email address:
+            <input
+              placeholder="Enter your Email"
+              className="border m-2"
+              onChange={(e) =>
+                setUserData((prev) => ({ ...prev, email: e.target.value }))
+              }
+              type="email"
+            />
+          </label>
+          <button
+            className="border border-spacing-3"
+            onClick={() => setOpenAddForm((prev) => !prev)}
           >
-            <button className="border border-spacing-3" type="submit">
-              Add user
-            </button>
-            <label>
-              User name:
-              <input
-                placeholder="Entetr a new username"
-                className="border m-2"
-                onChange={(e) =>
-                  setUserData((prev) => ({ ...prev, name: e.target.value }))
-                }
-                type="text"
-              />
-            </label>
-            <label>
-              Emailaddress:
-              <input
-                placeholder="Enter your Email"
-                className="border m-2"
-                onChange={(e) =>
-                  setUserData((prev) => ({ ...prev, email: e.target.value }))
-                }
-                type="email"
-              />
-            </label>
+            Close
+          </button>
+        </form>
+      ) : (
+        <div className="flex flex-col space-y-4">
+          {!session && (
             <button
-              className="border border-spacing-3"
+              className="border px-3 py-2 bg-black text-white rounded-md"
               onClick={() => setOpenAddForm((prev) => !prev)}
             >
-              Close
+              Add User
             </button>
-          </form>
-        ) : (
-          <div className="flex flex-col space-y-4">
-            {!session && (
-              <button
-                className="border px-3 py-2 bg-black text-white rounded-md"
-                onClick={() => setOpenAddForm((prev) => !prev)}
-              >
-                Add User
-              </button>
-            )}
-            {status === "loading" && "Loading..."}
-            {session ? (
-              <>
-                <a
-                  href="/api/auth/signout"
-                  className="border px-3 py-2 bg-black text-white rounded-md text-center"
-                >
-                  Logout
-                </a>
-                <button
-                  onClick={() => setOpenFormPost((prev) => !prev)}
-                  className="border px-3 py-2 bg-black text-white rounded-md text-center"
-                >
-                  Create Post
-                </button>
-                {openFormPost && (
-                  <form
-                    onSubmit={handleAddPost}
-                    className="flex grid-cols-4 space-x-10"
-                  >
-                    <label htmlFor="title">Title:</label>
-                    <input
-                      onChange={(e) =>
-                        setPostData((prev) => ({
-                          ...prev,
-                          title: e.target.value,
-                        }))
-                      }
-                      id="title"
-                      type="text"
-                      placeholder="Add Title"
-                      className="text-center col-span-4 border-black"
-                    />
-                    <label htmlFor="content">Post:</label>
-                    <input
-                      onChange={(e) =>
-                        setPostData((prev) => ({
-                          ...prev,
-                          content: e.target.value,
-                        }))
-                      }
-                      id="content"
-                      placeholder="Add Content"
-                      className="text-center col-span-4 border-black"
-                    />
-                    <button
-                      className="border px-3 py-2 bg-black text-white rounded-md text-center"
-                      type="submit"
-                    >
-                      Submit
-                    </button>
-                  </form>
-                )}
-                <div className="text-center py-8">
-                  <p>Wlecome {session?.user?.name}</p>
-                  <p>Here you can find your Posts</p>
-                </div>
-              </>
-            ) : (
+          )}
+          {status === "loading" && "Loading..."}
+          {session ? (
+            <>
               <a
-                href="/api/auth/signin"
+                href="/api/auth/signout"
                 className="border px-3 py-2 bg-black text-white rounded-md text-center"
               >
-                Login
+                Logout
               </a>
-            )}
-            <ul>
-              {users?.map((user) => (
-                <li
-                  key={user.id}
-                  onClick={() => handleDelete(user.id)}
-                  className="cursor-pointer"
+              <button
+                onClick={() => setOpenFormPost((prev) => !prev)}
+                className="border px-3 py-2 bg-black text-white rounded-md text-center"
+              >
+                Create Post
+              </button>
+              {openFormPost && (
+                <form
+                  onSubmit={handleAddPost}
+                  className="flex grid-cols-4 space-x-10"
                 >
-                  {user.name}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
-    </>
+                  <label htmlFor="title">Title:</label>
+                  <input
+                    onChange={(e) =>
+                      setPostData((prev) => ({
+                        ...prev,
+                        title: e.target.value,
+                      }))
+                    }
+                    id="title"
+                    type="text"
+                    placeholder="Add Title"
+                    className="text-center col-span-4 border-black"
+                  />
+                  <label htmlFor="content">Post:</label>
+                  <input
+                    onChange={(e) =>
+                      setPostData((prev) => ({
+                        ...prev,
+                        content: e.target.value,
+                      }))
+                    }
+                    id="content"
+                    placeholder="Add Content"
+                    className="text-center col-span-4 border-black"
+                  />
+                  <button
+                    className="border px-3 py-2 bg-black text-white rounded-md text-center"
+                    type="submit"
+                  >
+                    Submit
+                  </button>
+                </form>
+              )}
+              <div className="text-center py-8">
+                <p>Welcome {session?.user?.name}</p>
+                <p>Here you can find your Posts</p>
+              </div>
+              {status === "authenticated" && (
+                <ul>
+                  {posts?.map((post) => (
+                    <li key={post.id}>{post.title}</li>
+                  ))}
+                </ul>
+              )}
+            </>
+          ) : (
+            <a
+              href="/api/auth/signin"
+              className="border px-3 py-2 bg-black text-white rounded-md text-center"
+            >
+              Login
+            </a>
+          )}
+          <ul>
+            {users?.map((user) => (
+              <li
+                key={user.id}
+                onClick={() => handleDelete(user.id)}
+                className="cursor-pointer"
+              >
+                {user.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
   );
 }
